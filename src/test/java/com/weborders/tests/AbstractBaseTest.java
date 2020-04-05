6ee99879612e00ab651a27c0914e456c1a7b460c
erfan1336 <erfan1336@gmail.com>
2020-04-05 12:25:09 -0700

    second commit

--- src/test/java/com/weborders/tests/AbstractBaseTest.java
+++ src/test/java/com/weborders/tests/AbstractBaseTest.java
@@ -1,4 +1,41 @@
 package com.weborders.tests;
 
+import com.weborders.utilities.BrowserUtilities;
+import com.weborders.utilities.ConfigurationReader;
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 public class AbstractBaseTest {
+
+    protected WebDriver driver = Driver.getDriver();
+
+    @BeforeTest
+    public void beforeTest(){
+
+    }
+
+    @AfterTest
+    public void afterTest(){
+
+    }
+
+    @BeforeMethod
+    public void setup(){
+       driver.get(ConfigurationReader.getProperty("url"));
+       driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown(ITestResult testResult){
+        if (testResult.getStatus() == ITestResult.FAILURE){
+           String screenshotLocation = BrowserUtilities.getScreenshot(testResult.getName());
+        }
+
+        Driver.closeDriver();
+    }
 }
